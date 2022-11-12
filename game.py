f35dac23e5f1e7dc0d2d12eb4a302e16cadd275f
Will <slorkda@gmail.com>
2022-11-11 21:41:48 -0800

    Began deselect and toggle_select functions

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 import curses, runtime
 
+def deselect (x):
+	pass
+
 def handle_input (x):
 	if x == curses.KEY_UP or x == ord('k') or x == ord('w'):
 		runtime.index[1] = max(runtime.index[1] - 1, 0)
@@ -10,8 +13,11 @@ def handle_input (x):
 	elif x == curses.KEY_RIGHT or x == ord('l') or x == ord('d'):
 		runtime.index[0] = min(runtime.index[0] + 1, runtime.resolution_game[0] - 1)
 	elif x == ' ':
-		select(runtime.index)
+		toggle_select(runtime.index)
 
 def select (x):
+	pass
+
+def toggle_select (x):
 	#
 	pass
