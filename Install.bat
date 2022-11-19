2b747760906d3d37e26df8039340cd819b08a251
Will <slorkda@gmail.com>
2022-11-19 12:51:35 -0800

    Removed comment in batchfile

--- Install.bat
+++ Install.bat
@@ -1,4 +1,3 @@
-# Installer for Microsoft Windows
 mkdir C:\Temp
 Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.7.0/python-3.7.0.exe" -OutFile "C:\Temp\python-3.7.0.exe"
 C:\Temp\python-3.7.0.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
