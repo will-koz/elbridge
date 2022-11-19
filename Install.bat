d961198c75bd259943256fd528636dbee4985a4e
Will <slorkda@gmail.com>
2022-11-19 13:26:33 -0800

    Removed Install.bat

--- Install.bat
+++
@@ -1,4 +0,0 @@
-mkdir C:\Temp
-Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.7.0/python-3.7.0.exe" -OutFile "C:\Temp\python-3.7.0.exe"
-C:\Temp\python-3.7.0.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
-python.exe -m pip install -r .\requirements.txt
